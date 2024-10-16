66613127363b1448f7ee648b06d8c2ab74855f87
Matteo Lombardi <matteo.lombardi0606@gmail.com>
2024-10-16 22:47:15 +0200

    First Commit
    added portfolio on github

---
+++ script.js
@@ -0,0 +1,20 @@
+function toggleMenu() {
+  const menu = document.getElementById("menu");
+  const hamburger = document.querySelector(".hamburger");
+
+  menu.classList.toggle("active");
+  hamburger.classList.toggle("active");
+}
+
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+function opentab(tabname) {
+  for (tablink of tablinks) {
+    tablink.classList.remove("active-link");
+  }
+  for (tabcontent of tabcontents) {
+    tabcontent.classList.remove("active-tab");
+  }
+  event.currentTarget.classList.add("active-link");
+  document.getElementById(tabname).classList.add("active-tab");
+}
